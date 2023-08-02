4b1b9fe52052dae64a911755625a8f19b13bb214
Nistavilo <emirhankorkmaz275@gmail.com>
2023-08-02 15:08:27 +0300

    sfsdaf
    sdf

--- notification-challenge/src/App.vue
+++ notification-challenge/src/App.vue
@@ -1,30 +1,23 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Button from "./components/button.vue"
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
-
+ <div class="floor">
+  <Button></Button>
+  <Notification></Notification>
+ </div>
+ 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  .floor{  
+    position: absolute;
+    width: 700px;
+    height: 175px;
+
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    
+  }
 </style>
---
+++ notification-challenge/src/components/Button.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+2
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
